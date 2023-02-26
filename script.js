1deee24cc61a080e0d56685d9c33bbf1e8f68be4
RashaM <rashamassoud1998@gmail.com>
2023-02-26 03:27:50 +0200

    Saving the input values in JSON object

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+let fname ;
+let lname;
+
+let email;
+
+let password;
+let confirmPassword;
+
 function validateForm() {
-    location.href = 'waiting.html';
     // Get the form values
-    var fname = document.forms["registrationForm"]["fname"].value;
-    var lname = document.forms["registrationForm"]["lname"].value;
+    fname = document.forms["registrationForm"]["fname"].value;
+    lname = document.forms["registrationForm"]["lname"].value;
 
-    console.log(fname, lname);
-    var email = document.forms["registrationForm"]["email"].value;
-
-    var password = document.forms["registrationForm"]["password"].value;
-    var confirmPassword = document.forms["registrationForm"]["confirmPassword"].value;
+    email = document.forms["registrationForm"]["email"].value;
+    password = document.forms["registrationForm"]["password"].value;
+    confirmPassword = document.forms["registrationForm"]["confirmPassword"].value;
 
     // Check that all required fields are filled out
     if (fname == "" || lname == "" || email == "" || password == "" || confirmPassword == "") {
@@ -29,19 +34,32 @@ function validateForm() {
         return false;
     }
     if (password == confirmPassword) {
-        passwordStatus= CheckPassword(password);
+        passwordStatus = CheckPassword(password);
     }
 
     if (passwordStatus != true) {
         alert("The password do not meet the conditions of characters between 8 to 15 containing at least one lowercase letter, one uppercase letter, one numeric digit, and one special character.");
     }
 
+    const data = {
+        "firstName": fname,
+        "lastName": lname,
+        "email": email,
+        "password": password,
+    }
+    const jsontext = document.createElement("div");
+    jsontext.id = "jsontext";
+    document.body.appendChild(jsontext);
+    //convert JavaScript object to 
+    const myJSON= JSON.stringify(data);
+
+    location.href = 'waiting.html';
     // If all validation checks pass, submit the form
     return true;
 }
 
 function CheckPassword(password) {
-    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/ ;
+    const decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     if (password.match(decimal)) {
         return true;
     }
@@ -50,3 +68,6 @@ function CheckPassword(password) {
     }
 }
 
+function submitingData() {
+    location.href = "secondStep.html";
+}
